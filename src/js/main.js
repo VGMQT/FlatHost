28c56eecf7f31a223516c83f1918a249a9a710e1
Vlad Tepes <vg.email.2@gmail.com>
2018-06-25 17:42:51 +0300

    login modal added

--- src/js/main.js
+++ src/js/main.js
@@ -23,6 +23,14 @@ $(document).ready(function () {
             focusInput: false
         });
 
+        $('#modal-login').iziModal({
+            width: '25.5rem',
+            radius: 5,
+            overlayColor: 'rgba(0, 0, 0, 0.8)',
+            transitionIn: 'fadeInDown',
+            focusInput: false
+        });
+
         $(document).on('click', '.trigger', function (event) {
             event.preventDefault();
             $('#modal').iziModal('open');
@@ -31,6 +39,11 @@ $(document).ready(function () {
         $(document).on('click', '.trigger-proceed', function (event) {
             event.preventDefault();
             $('#modal-proceed').iziModal('open');
+        });
+
+        $(document).on('click', '.trigger-login', function (event) {
+            event.preventDefault();
+            $('#modal-login').iziModal('open');
         });
 
     }());
