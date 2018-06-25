5531d05541587f37642204b8e54ad10db315a8b7
Vlad Tepes <vg.email.2@gmail.com>
2018-06-25 17:36:57 +0300

    modals added

--- src/js/main.js
+++ src/js/main.js
@@ -5,4 +5,34 @@ $(document).ready(function () {
         svg4everybody();
     }());
 
+    //----------------------<<modal>>----------------------\\
+    (function () {
+
+        $('#modal').iziModal({
+            width: '25.5rem',
+            radius: 5,
+            overlayColor: 'rgba(0, 0, 0, 0.8)',
+            transitionIn: 'fadeInDown',
+            focusInput: false
+        });
+
+        $('#modal-proceed').iziModal({
+            width: '26.25rem',
+            radius: 5,
+            overlayColor: 'rgba(0, 0, 0, 0.8)',
+            focusInput: false
+        });
+
+        $(document).on('click', '.trigger', function (event) {
+            event.preventDefault();
+            $('#modal').iziModal('open');
+        });
+
+        $(document).on('click', '.trigger-proceed', function (event) {
+            event.preventDefault();
+            $('#modal-proceed').iziModal('open');
+        });
+
+    }());
+
 });
